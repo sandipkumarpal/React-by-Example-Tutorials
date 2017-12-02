455aaf9f5f85b66ec0a7016ec9d6ea2e4b5f4c8d
Sandip Pal <sandip.pal18@gmail.com>
2017-12-02 11:31:23 +0530

    Added ESLint package & Configaration

---
+++ react-example/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins" : [
+        "react",
+        "jsx-a11y",
+        "import",
+    ],
+    "env": {
+        "browser": true,
+        "node": true,
+        "jasmine": true
+    },
+    "rules": {
+        "react/prefer-stateless-function": [1, { "ignorePureComponents": true }],
+        "react/jsx-pascal-case": [2, { allowAllCaps: true }],
+        "jsx-closing-bracket-location": [1, "tag-aligned"],
+        "indent": ["error", 4],
+        "react/jsx-no-undef": [2, { "allowGlobals": true }],
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    }
+};
